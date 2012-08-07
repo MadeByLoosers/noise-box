cf8907ce19016890846722a5ecc90deebc12a4cd
jedrichards <jedrichards@gmail.com>
2012-08-07 12:47:33 +0100

    Initial model code commit.

---
+++ app/model/host-model.js
@@ -0,0 +1,37 @@
+var sys = require("sys");
+var events = require("events");
+
+var HostModel = function (id) {
+
+    this.id = id;
+    this.queue = [];
+    this.trackIndex = -1;
+
+    events.EventEmitter.call(this);
+};
+sys.inherits(HostModel, events.EventEmitter);
+
+HostModel.prototype = {
+
+    id : 0,
+    queue : undefined,
+    trackIndex : 0
+};
+
+HostModel.prototype.addTrack = function (track) {
+
+    this.queue.push(track);
+};
+
+HostModel.prototype.notifyTrackStarted = function () {
+
+
+};
+
+HostModel.prototype.notifyTrackComplete = function () {
+
+};
+
+module.exports = HostModel;
+
+// var host = new Host(id,[])
