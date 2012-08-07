dbafc6e6f75fbc66b078ece972201906db4b9daf
k0nG <mailmattcollins@gmail.com>
2012-08-07 15:47:29 +0100

    Playing audio. Finished event.

--- public/js/noise-box.js
+++ public/js/noise-box.js
@@ -3,35 +3,92 @@
 */
 var NB = {};
 
-
+/**
+* Initialize Socket IO.
+* returns true|false 
+*/
 NB.initSocket = function(){
 
-	NB.socket = io.connect('http://'+window.location.hostname+':3000');
+  //Do we have socket io included?
+  if(typeof io === 'object'){
 
-  NB.socket.on('message', function(data){
-    console.log('received message: ', data);
-  });
+    //Connect to local host
+  	NB.socket = io.connect('http://'+window.location.hostname+':3000');
+
+    //Default message recieved.
+    NB.socket.on('message', function(data){
+      console.log('received message: ', data);
+    });
+
+    return true;
+  }else{
+
+    console.error('socket.io has not loaded correctly.');
+
+    return false;
+  }
 
 };
 
+/**
+* Entry point for Host page.
+*/
+NB.initHostPage = function(){
+
+  //Notify backend of new host
+  NB.socket.emit('host', { name: 'test' });
+
+  //Wait for play commands
+  NB.socket.on('play', function(data){
+    var path = data.content.path;
+    console.log(path);
+    //Play
+    NB.player.play(path, function(){
+      NB.socket.emit('finishedPlay', {path:path});
+    });
+
+
+  });
+}
+
+
+NB.player = (function() {
+  'use strict';
+
+  var player = {
+    element: document.getElementById('audioPlayer'),
+    play: function (fileUrl, completeCallback) {
+      this.element.addEventListener('ended', function(){
+        completeCallback();
+      })
+      this.element.src = fileUrl;
+      this.element.play();
+    }
+  };
+
+  return player;
+
+}());
 
 
 $(document).ready(function(e){
 	
-  NB.initSocket();
+  if(NB.initSocket()){
 
-  switch($('body').attr('id')){
-    case 'home':
+    switch($('body').attr('id')){
+      case 'home':
 
-    break;
+      break;
 
-    case 'host':
-      NB.socket.emit('host', { name: 'test' });
-    break;
+      case 'host':
+        NB.initHostPage();
+      break;
 
-    case 'room':
-      NB.socket.emit('join', { name: 'test '});
-    break;
+      case 'room':
+        NB.socket.emit('join', { name: 'test '});
+      break;
+
+    }
 
   }
 
