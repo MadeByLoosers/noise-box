130313b80fa287d13289dde3f1a6d4048f2de252
Mike Allanson <michael.allanson@gmail.com>
2012-08-07 12:25:25 +0100

    Websocket test

--- server.js
+++ server.js
@@ -2,17 +2,22 @@
  * Noise Box
  */
 
+
 var express = require('express'),
     http = require('http');
 
 var app = module.exports = express();
 
+// io needs an http server instance: http://bit.ly/vxOxL5
+var server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
 // import app config
 require('./app/env/env')(app, express);
 require('./app/routes/routes')(app);
-require('./app/app')(app);
+require('./app/app')(app, io);
 
 // start the app
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
