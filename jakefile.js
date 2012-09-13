173d7dca1e31ff8fcdc1457b21fcb64042ad1c95
Mike Allanson <michael.allanson@gmail.com>
2012-09-13 12:11:17 +0100

    Intitial support for Lint

--- jakefile.js
+++ jakefile.js
@@ -1,10 +1,18 @@
-task("default", [], function(){
+task("default", ["lint"], function(){
     console.log("default");
 });
 
-desc["Example!"];
+desc("lint everything");
+task("lint", [], function () {
+    console.log("Linting files");
+
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("jakefile.js", {});
+});
+
+desc("Example!");
 task("example", ["dependency"], function(){
-    console.log("example task")
+    console.log("example task");
 });
 
 task("dependency", function(){
