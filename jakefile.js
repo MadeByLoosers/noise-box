96c992fa5aa9ef6ac299b72654139158be5d0f1b
Mike Allanson <michael.allanson@gmail.com>
2012-09-13 11:56:47 +0100

    added build system

---
+++ jakefile.js
@@ -0,0 +1,12 @@
+task("default", [], function(){
+    console.log("default");
+});
+
+desc["Example!"];
+task("example", ["dependency"], function(){
+    console.log("example task")
+});
+
+task("dependency", function(){
+    console.log("dependency");
+});
