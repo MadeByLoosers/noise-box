6ab7b56262187bb23adf6d16aba571ba4fd8c2f0
Mike Allanson <michael.allanson@gmail.com>
2013-01-09 14:28:31 +0000

    Allow the noise box app to end itself

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * NoiseBox
+ * index.js
+ *
+ * Starts up the server
+ */
+
+var NBApp = require("./server");
+
+NBApp.startApp();
--- src/server.js
+++ src/server.js
@@ -6,66 +6,75 @@
  * Main application entry point.
  */
 
-// Pull in dependancies:
+var startApp = function () {
+	// Pull in dependancies:
 
-var express = require("express");
-var expressPartials = require("express-partials");
-var socketIO = require("socket.io");
+	var express = require("express");
+	var expressPartials = require("express-partials");
+	var socketIO = require("socket.io");
 
-var constants = require("./public/js/const");
-var error = require("./app/middleware/error");
-var AppModel = require("./app/model/AppModel");
+	var constants = require("./public/js/const");
+	var error = require("./app/middleware/error");
+	var AppModel = require("./app/model/AppModel");
 
-// Detect environment:
+	// Detect environment:
 
-var port = process.env.PORT || ( process.argv[2] || constants.DEFAULT_PORT );
-var env = process.env.NODE_ENV || constants.DEV;
+	var port = process.env.PORT || ( process.argv[2] || constants.DEFAULT_PORT );
+	var env = process.env.NODE_ENV || constants.DEV;
 
-// Create and configure an Express app instance:
+	// Create and configure an Express app instance:
 
-var app = express();
+	var app = express();
 
-app.set("port",port);
-app.set("views",__dirname + "/app/views");
-app.set("view engine","ejs");
+	app.set("port",port);
+	app.set("views",__dirname + "/app/views");
+	app.set("view engine","ejs");
 
-// Middleware:
+	// Middleware:
 
-app.use(express.favicon());
-app.use(express.logger(env===constants.DEV?"dev":"default"));
-app.use(express.static(__dirname + "/public"));
-app.use(expressPartials());
-app.use(express.cookieParser("N01ZEBOXX"));
-app.use(express.cookieSession({key:"sid"}));
-app.use(express.bodyParser());
-app.use(app.router);
-app.use(error());
+	app.use(express.favicon());
+	app.use(express.logger(env===constants.DEV?"dev":"default"));
+	app.use(express.static(__dirname + "/public"));
+	app.use(expressPartials());
+	app.use(express.cookieParser("N01ZEBOXX"));
+	app.use(express.cookieSession({key:"sid"}));
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.use(error());
 
-// Start the Express server:
+	// Start the Express server:
 
-var server = app.listen(port,function () {
+	var server = app.listen(port,function () {
 
-    console.log("NoiseBox (%s) started on port %d at %s",env,port,new Date().toString());
-});
+	    console.log("NoiseBox (%s) started on port %d at %s",env,port,new Date().toString());
+	});
 
-// Start socket.io:
+	// Start socket.io:
 
-var io = socketIO.listen(server,{"log level":0});
+	var io = socketIO.listen(server,{"log level":0});
 
-// Expose app actors:
+	// Expose app actors:
 
-module.exports.constants = constants;
-module.exports.app = app;
-module.exports.io = io;
-module.exports.env = env;
-module.exports.port = port;
-module.exports.model = new AppModel();
+	module.exports.constants = constants;
+	module.exports.app = app;
+	module.exports.io = io;
+	module.exports.env = env;
+	module.exports.port = port;
+	module.exports.model = new AppModel();
 
-// Init controllers:
+	// Init controllers:
 
-var HomeController = require("./app/controllers/home");
-var HostController = require("./app/controllers/host");
-var UserController = require("./app/controllers/user");
-HomeController();
-HostController.init();
-UserController.init();
+	var HomeController = require("./app/controllers/home");
+	var HostController = require("./app/controllers/host");
+	var UserController = require("./app/controllers/user");
+	HomeController();
+	HostController.init();
+	UserController.init();
+};
+
+var stopApp = function(){
+	process.exit();
+};
+
+exports.startApp = startApp;
+exports.stopApp = stopApp;
