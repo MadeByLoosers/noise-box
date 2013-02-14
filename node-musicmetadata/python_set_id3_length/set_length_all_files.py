45ad3d6d6ddb3abdfa1723af05a2ff3853bb6167
Pete Graham <petegraham1@gmail.com>
2013-02-14 16:41:47 +0000

    id3 tags set

--- node-musicmetadata/python_set_id3_length/set_length_all_files.py
+++ node-musicmetadata/python_set_id3_length/set_length_all_files.py
@@ -15,22 +15,29 @@ def set_length_all_files(path):
 
 
 def set_id3_length(file_path):
-    audio = MP3(file_path)
-    # get the seconds as a rounded number
-    length = int(audio.info.length)
-    # format the length hh:mm:ss
-    length = str(datetime.timedelta(seconds=length))
-    # format time further
-    if length[0:3] == '0:0':
-        length = length[3:]
-    elif length[0:2] == '0:':
-        length = length[2:]
-    #print length
+    try:
+        #TODO: set title to file (remove .mp3 remove underscores)
+        #TODO: album to folder name
+        #TODO: remove artist
+        #TODO: remove genre
+        audio = MP3(file_path)
+        # get the seconds as a rounded number
+        length = int(audio.info.length)
+        # format the length hh:mm:ss
+        if length >= 60:
+            length = str(datetime.timedelta(seconds=length))
+            # format time further
+            if length[0:3] == '0:0':
+                length = length[3:]
+            elif length[0:2] == '0:':
+                length = length[2:]
+        #print length
 
-    audio_id3 = EasyID3(file_path)
-    audio_id3["length"] = length
-    audio_id3.save()
+        audio_id3 = EasyID3(file_path)
+        audio_id3["length"] = str(length)
+        audio_id3.save()
+    except Exception as e:
+        print 'EXCEPTION %s %s' % (file_path, e)
 
-#set_id3_length('/Users/pxg/Sites/noisebox/src/public/sfx/adam-and-joe/pirate-interruptions-chocolate-cake-slice.mp3')
-set_length_all_files('/Users/pxg/Sites/noisebox/src/public/sfx/adam-and-joe/')
+set_id3_length('/Users/pxg/Sites/noisebox/src/public/sfx/_misc/destroy.mp3')
 #set_length_all_files('/Users/pxg/Sites/noisebox/src/public/sfx/')
