d398f3ede137a9d648476980fdd0c8882619af26
Pete Graham <petegraham1@gmail.com>
2013-02-14 16:11:05 +0000

    converting seconds to minutes and hours

--- node-musicmetadata/python_set_id3_length/set_length_all_files.py
+++ node-musicmetadata/python_set_id3_length/set_length_all_files.py
@@ -1,14 +1,20 @@
-from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+import datetime
 
 
 def set_id3_length(file_path):
     audio = MP3(file_path)
-    length = audio.info.length
+    # get the seconds as a rounded number
+    length = int(audio.info.length)
+    # format the length
+    length = str(datetime.timedelta(seconds=length))
+    print length
+    #length = "1:00:01"
+    #print length
+
     audio_id3 = EasyID3(file_path)
-    audio_id3["length"] = str(int(length))
+    audio_id3["length"] = length
     audio_id3.save()
 
 set_id3_length('/Users/pxg/Sites/noisebox/src/public/sfx/adam-and-joe/pirate-interruptions-chocolate-cake-slice.mp3')
-#TODO: round to nearest second
-#TODO: can we set the time as minutes:seconds?
