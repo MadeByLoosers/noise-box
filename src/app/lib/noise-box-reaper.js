d489d1e8a3942744309eac4abb84e09c98273a69
jedrichards <jedrichards@gmail.com>
2013-03-04 22:11:49 +0000

    Fixing null object ref error that occured at every stale 'box reaping

--- src/app/lib/noise-box-reaper.js
+++ src/app/lib/noise-box-reaper.js
@@ -1,4 +1,5 @@
-var model = require("../../server").model;
+var server = require("../../server");
+var log = require("./log");
 
 var reapInterval = 1000*60*60*12;   // Reap empty boxes every 12 hours
 var staleness = 1000*60*60*6;       // Spare a box from a reaping if its had activity within the last 6 hours
@@ -6,13 +7,14 @@ var staleness = 1000*60*60*6;       // Spare a box from a reaping if its had act
 module.exports = function () {
     setInterval(function () {
         var toRemove = [];
-        model.noiseBoxes.each(function (noiseBox) {
+        server.model.noiseBoxes.each(function (noiseBox) {
             if ( Date.now() > (noiseBox.activityTimestamp+staleness) ) {
                 toRemove.push(noiseBox);
             }
         });
+        if ( toRemove.length > 0 ) log.info("now reaping "+toRemove.length+" stale boxes");
         toRemove.forEach(function (noiseBox) {
-            model.noiseBoxes.remove(noiseBox);
+            server.model.noiseBoxes.remove(noiseBox);
         });
     },reapInterval);
 };
